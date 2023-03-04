376ce6f444427ef4f63fe00ef0c0baafa9acf2c6
Dora Tzeng <dora.tzeng@gmail.com>
2023-03-03 17:27:13 -0800

    upload multiple files

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -15,6 +15,12 @@ if ! which curl > /dev/null; then
     exit 1
 fi
 
+all_files=()
+for filename in ${ASSETS}; do
+  all_files+=(-F "file[]=@$filename")
+done
+echo "${all_files[@]}"
+
 URL="https://upload.aviator.co/api/test-report-uploader"
 REPO_URL="https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
 
@@ -26,7 +32,7 @@ response=$(curl -X POST -H "x-Aviator-Api-Key: ${AVIATOR_API_TOKEN}" \
 	-H "Commit-Sha: ${CIRCLE_SHA1}" \
 	-H "Repo-Url: $REPO_URL" \
 	-H "Branch-Name: ${CIRCLE_BRANCH}" \
-	-F "file=@${ASSETS}" \
+	"${all_files[@]}" \
 	"$URL") || true
 
 echo "$response"
